9b1fd5574cdf9ec109fc0ba1c1123228be609579
Yuvi <Yuvaraj.Selvarajan@shell.com>
2024-04-23 01:11:15 +0530

    minor improvements in the navbar

---
+++ src/data/navbar-data.tsx
@@ -0,0 +1,20 @@
+const links = [
+  {
+    name: "Home",
+    url: "#",
+  },
+  {
+    name: "About",
+    url: "#about",
+  },
+  {
+    name: "Skills",
+    url: "#skills",
+  },
+  {
+    name: "Certifications",
+    url: "#certifications",
+  },
+];
+
+export default links;
