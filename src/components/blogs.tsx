263f940de6a914190dd577c05c6749dce2520241
Yuvi <Yuvaraj.Selvarajan@shell.com>
2024-04-23 14:12:05 +0530

    fixed the width issues in blog

--- src/components/blogs.tsx
+++ src/components/blogs.tsx
@@ -20,14 +20,14 @@ export default function Blogs() {
       id="blogs"
       className="min-h-screen flex flex-col items-center px-4 py-16 bg-fuchsia-400"
     >
-      <h1 className="scroll-m-20 text-4xl font-extrabold text-center tracking-widest mx-96 mt-8 lg:text-5xl pt-8">
+      <h1 className="scroll-m-20 text-4xl font-extrabold text-center tracking-widest md:mx-96 mt-12 lg:text-5xl md:pt-8">
         BLOGS
       </h1>
-      <p className="text-md leading-relaxed mb-6 tracking-wide w-2/5 pt-4">
+      <p className="text-md leading-relaxed mb-6 tracking-wide pt-4">
         Here you will find some of my blogs, mostly about technology and
         programming. I hope you find them useful.
       </p>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-48">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:px-48">
         {allBlogs
           .sort((a, b) => {
             if (
@@ -40,7 +40,7 @@ export default function Blogs() {
           })
           .map((post, index) => (
             <div key={index} className="flex">
-              <Card className="hover:drop-shadow-2xl">
+              <Card className="hover:drop-shadow-2xl w-screen">
                 <CardHeader>
                   <CardTitle className="font-bold tracking-wider text-center hover:text-green-500">
                     <Link href={`/blogs/${post.slug}`}>
